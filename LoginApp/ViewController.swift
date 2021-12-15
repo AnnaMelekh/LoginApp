0685ae36b18b587251586df1afbdda9b8e04dab5
Anna <annasmelekhina@gmail.com>
2021-12-16 00:37:37 +0300

    added gradient and hiding of keyboard

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -13,10 +13,11 @@ class ViewController: UIViewController {
     @IBOutlet var textUser: UITextField!
     @IBOutlet var textPass: UITextField!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setGradientBackground()
     }
-    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let inputText = textUser.text, !inputText.isEmpty else {
@@ -27,6 +28,11 @@ class ViewController: UIViewController {
         hiVC.name = textUser.text
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
     @IBAction func LoginButton() {
         if textUser.text != "Name" && textPass.text != "Password" {
             showAlert(title: "Error", message: "Wrong name or pass")
@@ -49,6 +55,19 @@ class ViewController: UIViewController {
         textPass.text = nil
 
     }
+    
+    func setGradientBackground() {
+           let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+           let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+                       
+           let gradientLayer = CAGradientLayer()
+           gradientLayer.colors = [colorTop, colorBottom]
+           gradientLayer.locations = [0.0, 1.0]
+           gradientLayer.frame = self.view.bounds
+                   
+           self.view.layer.insertSublayer(gradientLayer, at:0)
+       }
+    
 }
 
 
