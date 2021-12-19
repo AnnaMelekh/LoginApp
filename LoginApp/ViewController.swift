f797aadf155673afcde7bc7b0ca6bc3fc332d1c2
Anna <annasmelekhina@gmail.com>
2021-12-19 19:41:43 +0300

    fixes + Models

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -18,18 +18,9 @@ class ViewController: UIViewController {
         hiVC.name = textUser.text
     }
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        super.touchesBegan(touches, with: event)
-        view.endEditing(true)
-    }
-    
     
     @IBAction func LoginButton() {
-        guard let inputText = textUser.text, !inputText.isEmpty else {
-            showAlert(title: "Error", message: "Wrong name")
-            return
-        }
-
+        
         if textUser.text != "Name", textPass.text != "Password" {
             showAlert(title: "Error", message: "Wrong name or pass")
         } else if textUser.text == "Name" && textPass.text == "Password" {
@@ -46,23 +37,9 @@ class ViewController: UIViewController {
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        let _ = segue.source as! HiViewController
         textUser.text = nil
         textPass.text = nil
-
     }
-    
-    func setGradientBackground() {
-           let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
-           let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
-                       
-           let gradientLayer = CAGradientLayer()
-           gradientLayer.colors = [colorTop, colorBottom]
-           gradientLayer.locations = [0.0, 1.0]
-           gradientLayer.frame = self.view.bounds
-                   
-           self.view.layer.insertSublayer(gradientLayer, at:0)
-       }
 }
 
 
@@ -72,6 +49,27 @@ extension ViewController {
         let okAction = UIAlertAction(title: "Ok", style: .default)
         alert.addAction(okAction)
         present (alert, animated: true)
+    }
+}
+
+extension ViewController {
+    private func setGradientBackground() {
+        let colorTop =  UIColor(red: 255.0/255.0, green: 149.0/255.0, blue: 0.0/255.0, alpha: 1.0).cgColor
+        let colorBottom = UIColor(red: 255.0/255.0, green: 94.0/255.0, blue: 58.0/255.0, alpha: 1.0).cgColor
+                    
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [colorTop, colorBottom]
+        gradientLayer.locations = [0.0, 1.0]
+        gradientLayer.frame = self.view.bounds
+                
+        self.view.layer.insertSublayer(gradientLayer, at:0)
+    }
+}
+
+extension ViewController {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
 }
 
