4cca2c5dc2b15e03c6893937a8ef22fd27686545
Anna <annasmelekhina@gmail.com>
2021-12-20 10:09:46 +0300

    fixes

--- LoginApp/ViewControllers/AboutViewController.swift
+++ LoginApp/ViewControllers/AboutViewController.swift
@@ -3,11 +3,11 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-    @IBOutlet var avatar: UILabel!
+    @IBOutlet var avatar: UILabel! = UILabel()
     
-    @IBOutlet var name: UILabel!
+    @IBOutlet var name: UILabel! = UILabel()
     
-    @IBOutlet var age: UILabel!
+    @IBOutlet var age: UILabel! = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,9 +15,6 @@ class AboutViewController: UIViewController {
         avatar.text = userAnna.person.photo
         name.text = "Name: " + userAnna.person.name
         age.text = "Age: " + userAnna.person.age
-        
     }
     
-
-
 }
